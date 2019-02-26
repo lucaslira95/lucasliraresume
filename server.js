0583a3a37872b0b76b51235df19417c0a40ae465
Lucas Lira <lucasgta95@gmail.com>
2019-02-26 02:23:02 -0300

    added server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/css", express.static(__dirname));
+app.use("/img", express.static(__dirname + '/img'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/scss", express.static(__dirname + '/scss'));
+app.use("/vendor", express.static(__dirname + '/vendor'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
